eab9b2776555bea4b9056286bfd84c1b88ffb44e
Harald Hoyer <hh@matterlabs.com>
2023-07-14 10:09:10 +0200

    feat: package github.com/intel/SGXDataCenterAttestationPrimitives
    Signed-off-by: Harald Hoyer <hh@matterlabs.com>
    Signed-off-by: Harald Hoyer <harald@hoyer.xyz>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27487,6 +27487,8 @@ with pkgs;
 
   libnl-tiny = callPackage ../os-specific/linux/libnl-tiny { };
 
+  libsgx-dcap-quote-verify = callPackage ../os-specific/linux/sgx/dcap-quote-verify { };
+
   libtraceevent = callPackage ../os-specific/linux/libtraceevent { };
 
   libtracefs = callPackage ../os-specific/linux/libtracefs { };
@@ -28148,6 +28150,8 @@ with pkgs;
   sgx-ssl = callPackage ../os-specific/linux/sgx/ssl { };
 
   sgx-psw = callPackage ../os-specific/linux/sgx/psw { };
+
+  sgx-dcap = callPackage ../os-specific/linux/sgx/dcap { };
 
   shadow = callPackage ../os-specific/linux/shadow { };
 
